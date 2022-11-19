29d65ca87b701df6175ea5841c59805b8048f7a0
Luismanuel1996 <luismanuel1996@live.com>
2022-11-19 01:56:36 -0500

    got api to display

--- sandbox.js
+++ sandbox.js
@@ -1,60 +1,30 @@
-const city = document.getElementById("city");
 
-
-function getData(cityname){
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=2&appid=${apiKey}`)
-    .then((resolve) => {
-        return resolve.json()
-    })
-    .then((data) => {
-    city.value = "";
-    let weatherData = data[0];
-    const lonLat = {lon: weatherData.lon , lat: weatherData.lat};
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lonLat.lon}&lon=${lonLat.lat}&appid=${apiKey}`)
-     })
-    .then((resolve) =>{
-     return resolve.json();
-    }).then((data) =>{
-        console.log(data)
-        
-    })
-    .catch((error) => {
-        console.error= error.message;
-    });
-}
-
-
-function getWeather(lonLat){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lonLat.lon}&lon=${lonLat.lat}&appid=${apiKey}`)
-    .then((resolve)=>{
-        return resolve.json();
-    }).then((data)=>{
-        return console.log(data)
-
-})
-}
-
-searchButton.addEventListener("submit", (event) =>{
-    event.preventDefault();
-    getData(cityname);
-})
-
-
-
-function getData(cityname){
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=2&appid=${apiKey}`)
-    .then((resolve) =>{
-        return resolve.json();
-    })
-    .then((data)=> {
-        city.value = "";
-        let weatherData = data[0];
-        const lonLat = {lon: weatherData.lon , lat: weatherData.lat}
-        return lonLat;
-        
-    })
-    .catch((error) => {
-        console.error= error.message;
-    });
-}
-
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let cityRef = document.getElementById("city");
+//Function to fetch weather details from api and display them
+let getWeather = () => {
+  let cityValue = cityRef.value;
+  //If input field is empty
+  if (cityValue.length == 0) {
+    result.innerHTML = <h3 class="msg">Please enter a city name</h3>;
+  }
+  //If input field is NOT empty
+  else {
+    let url = https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric;
+    //Clear the input field
+    cityRef.value = "";
+    fetch(url)
+      .then((resp) => resp.json())
+      //If city name is valid
+      .then((data) => {
+        console.log(data);
+        console.log(data.weather[0].icon);
+        console.log(data.weather[0].main);
+        console.log(data.weather[0].description);
+        console.log(data.name);
+        console.log(data.main.temp_min);
+        console.log(data.main.temp_max);
+        result.innerHTML = 
+  }
+};
--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 const apiKey = `c33ac7f507cb7cfdacc8ab0e441d0c2d`;
 const feedBack = document.getElementById("feedBack");
+const currentCity = document.getElementById("cityName");
+const currentTemp = document.getElementById("currentTemp");
 const searchButton = document.getElementById("button");
 const city = document.getElementById("city");
 const card = document.getElementById("card");
-console.log("w.e");
+const feelsLike = document.getElementById("feelsLike");
+
 
 card.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("something");
   const cityname = city.value;
   getData(cityname);
 });
 
 function getData(cityname) {
-  console.log("w.e");
   fetch(
-    `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=2&appid=${apiKey}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=2&appid=${apiKey}&units=imperial`
   )
     .then((resolve) => resolve.json())
     .then((data) => {
       city.value = "";
       let wData = data[0];
       const lonLat = { lat: wData.lat, lon: wData.lon };
-      console.log(lonLat);
       return fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lonLat.lat}&lon=${lonLat.lon}&appid=${apiKey}`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lonLat.lat}&lon=${lonLat.lon}&appid=${apiKey}&units=imperial`
       );
     })
     .then((resolve) => {
       return resolve.json();
     })
     .then((data) => {
-      console.log(data);
+      console.log(data)
+      currentCity.textContent = `Currently in ${data.name}`;
+      feedBack.textContent = `Visibilty: ${data.weather[0].description}`;
+      currentTemp.textContent = `The current temp is ${data.main.temp} °Degrees `;
+      feelsLike.textContent = `Feels like ${data.main.feels_like} °Degrees`;
+
     })
     .catch((error) => {
       console.error = error.message;
